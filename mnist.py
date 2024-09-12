31c493274682af27a1b0b5343a9ffa518bc64d38
Léo Paillé <leo.paille@enseirb-matmeca.fr>
2024-09-12 16:56:40 +0200

    it's training !

--- mnist.py
+++ mnist.py
@@ -1,7 +1,8 @@
 import hashlib, urllib, pathlib, tempfile, gzip, os
-import numpy as np
 import torch
-from torch import nn, Tensor, optim
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 from typing import Optional, Union
 
 # modified snippet from https://github.com/tinygrad/tinygrad/blob/master/tinygrad/helpers.py#L266
@@ -38,7 +39,8 @@ def mnist(device=None):
             _mnist("t10k-labels-idx1-ubyte.gz", "test_y")[8:].to(device)
 
 
-X_train, Y_train, X_test, Y_test = mnist()
+X_train, Y_train, X_test, Y_test = mnist(device="cuda")
+Y_train, Y_test = Y_train.to(torch.long), Y_test.to(torch.long)
 
 # import matplotlib.pyplot as plt
 # print(Y_train[0])
@@ -48,19 +50,21 @@ X_train, Y_train, X_test, Y_test = mnist()
 model = nn.Sequential(
             nn.Conv2d(1, 32, 5), nn.ReLU(),
             nn.Conv2d(32, 32, 5), nn.ReLU(),
-            nn.BatchNorm1d(32), nn.MaxPool2d((2, 2)),
+            nn.BatchNorm2d(32), nn.MaxPool2d((2, 2)),
             nn.Conv2d(32, 64, 3), nn.ReLU(),
             nn.Conv2d(64, 64, 3), nn.ReLU(),
-            nn.BatchNorm1d(64), nn.MaxPool2d((2, 2)),
+            nn.BatchNorm2d(64), nn.MaxPool2d((2, 2)),
             nn.Flatten(1), nn.Linear(576, 10))
+model.to("cuda")
 opt = optim.Adam(model.parameters())
-loss_fn = nn.CrossEntropyLoss(reduction="none")
 
 test_acc = float("nan")
 for i in range(70):
     opt.zero_grad()
-    samples = torch.randint(0, X_train.shape[0], (512,))
+    samples = torch.randint(0, X_train.shape[0], (512,)).cuda()
     # TODO: this "gather" of samples is very slow. will be under 5s when this is fixed
-    loss = loss_fn(model(X_train[samples].reshape(512, 28, 28)), Y_train[samples]).backward()
+    loss = F.nll_loss(model(X_train[samples]), Y_train[samples])
+    loss.backward()
     opt.step()
+    if i%10 == 9: test_acc = (torch.argmax(model(X_test), dim=1) == Y_test).to(torch.float).mean() * 100
     print(f"loss: {loss.item():6.2f} test_accuracy: {test_acc:5.2f}%")
